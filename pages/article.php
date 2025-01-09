279671bce08d80a3a60ca3c648207493c8e68e08
fatima-ezzahraalouane <falouane38@gmail.com>
2025-01-09 15:48:23 +0100

    #10 #18 afficher les articles a travers database

--- pages/article.php
+++ pages/article.php
@@ -6,14 +6,14 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 2) {
 }
 
 require '../config/Database.php';
-// require '../classes/Theme.php';
+require '../classes/Article.php';
 
-// $database = new Database();
-// $conn = $database->getConnection();
+$database = new Database();
+$conn = $database->getConnection();
 
-// $theme = new Theme($conn);
+$article = new Article($conn);
 
-// $themes = $theme->getAllThemes();
+$articles = $article->getAllArticlesWithComments();
 ?>
 
 
@@ -142,29 +142,31 @@ require '../config/Database.php';
     <div class="container-fluid blog py-5">
         <div class="container py-5">
             <div class="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 800px;">
-                <h1 class="display-5 text-capitalize mb-3">Cental<span class="text-primary"> Blog & News</span></h1>
-                <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut amet nemo expedita asperiores commodi accusantium at cum harum, excepturi, quia tempora cupiditate! Adipisci facilis modi quisquam quia distinctio,
+                <h1 class="display-5 text-capitalize mb-3">Articles<span class="text-primary"> Drive & Loc</span></h1>
+                <p class="mb-0">Explorez nos articles soigneusement sélectionnés pour enrichir vos expériences de location de voiture</p>
                 </p>
             </div>
             <div class="row g-4">
-                <div class="col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="blog-item">
-                        <div class="blog-img">
-                            <img src="../assets/img/blog-1.jpg" class="img-fluid rounded-top w-100" alt="Image">
-                        </div>
-                        <div class="blog-content rounded-bottom p-4">
-                            <div class="blog-date">30 Dec 2025</div>
-                            <div class="blog-comment my-3">
-                                <div class="small"><span class="fa fa-user text-primary"></span><span class="ms-2">Martin.C</span></div>
-                                <div class="small"><span class="fa fa-comment-alt text-primary"></span><span class="ms-2">6 Comments</span></div>
+                <?php foreach ($articles as $a): ?>
+                    <div class="col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="blog-item">
+                            <div class="blog-img">
+                                <img src="<?= htmlspecialchars($a['image_url']) ?>" class="img-fluid rounded-top w-100" alt="Image">
                             </div>
-                            <a href="#" class="h4 d-block mb-3">Rental Cars how to check driving fines?</a>
-                            <p class="mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius libero soluta impedit eligendi? Quibusdam, laudantium.</p>
-                            <a href="#" class="">Read More <i class="fa fa-arrow-right"></i></a>
+                            <div class="blog-content rounded-bottom p-4">
+                                <div class="blog-date"><?= date('d M Y', strtotime($a['date_creation'])) ?></div>
+                                <div class="blog-comment my-3">
+                                    <div class="small"><span class="fa fa-user text-primary"></span><span class="ms-2"><?= htmlspecialchars($a['id_user']) ?></span></div>
+                                    <div class="small"><span class="fa fa-comment-alt text-primary"></span><span class="ms-2"><?= $a['comment_count'] ?> Comments</span></div>
+                                </div>
+                                <a href="#" class="h4 d-block mb-3"><?= htmlspecialchars($a['titre']) ?></a>
+                                <p class="mb-3"><?= substr(htmlspecialchars($a['contenu']), 0, 50) ?>...</p>
+                                <a href="view_article.php?id=<?= $a['id_article'] ?>" class="">Read More <i class="fa fa-arrow-right"></i></a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
+                <?php endforeach; ?>
+                <!-- <div class="col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
                     <div class="blog-item">
                         <div class="blog-img">
                             <img src="../assets/img/blog-2.jpg" class="img-fluid rounded-top w-100" alt="Image">
@@ -197,7 +199,7 @@ require '../config/Database.php';
                             <a href="#" class="">Read More <i class="fa fa-arrow-right"></i></a>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
